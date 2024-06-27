16788102f6b16e2c5d05af08d7119fb3d526fef5
Wooyeon Lee <wooyeon0.lee@samsung.com>
2024-06-27 16:33:22 +0900

    log

--- vllm/spec_decode/spec_decode_worker.py
+++ vllm/spec_decode/spec_decode_worker.py
@@ -429,6 +429,8 @@ class SpecDecodeWorker(LoraNotSupportedWorkerBase):
             proposals,
         )
 
+        logger.info("DECODING STEP. VERIFY")
+
         accepted_token_ids, target_logprobs = self._verify_tokens(
             execute_model_req.seq_group_metadata_list, proposal_scores,
             proposals, execute_model_req.num_lookahead_slots)
